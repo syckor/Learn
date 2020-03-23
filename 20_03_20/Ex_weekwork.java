67c3c3f188c5f4d543224c8cc1dffdc58847937b
syckor <syckor@naver.com>
2020-03-23 09:19:07 +0900

    completed weekwork
    Finish!

--- 20_03_20/Ex_weekwork.java
+++ 20_03_20/Ex_weekwork.java
@@ -22,50 +22,23 @@ public class Ex_weekwork {
 			System.out.println("테스트결과를 입력하세요");
 			test[i] = input.nextLine().toCharArray();   //String을 char array로 변환해줌
 			                                            //여기까지 해서 test배열을 완료   
-			// 1+ 2 + 3 + 4 + .....
-//			if(ch[i]=='O')
-//			{
-//			count++;
-//			for(int j=1; j==ch.length;j++)
-//			{
-//			if(ch[i]==ch[j])
-//			{
-//			count++;
-//			}
-//			}
-//			}
-//			else
-//			{
-//			count=0;
-//			}
-//			total=total+count;
-//			}
-//			System.out.println(total);
-//			}
-			int result = 0; 
+
+			int sum = 0;
 			int o = 0;
-			for ( int j = 0; j < test[i].length ; j++) {   //test[i][j]
-				
-//				int sum = 0;
-//				for (int i = 1 ; i <= 100; i++) {
-//					sum += i;
+			for ( int j = 0; j < test[i].length ; j++) {   //test[i][j]		
 				
 				if(test[i][j] == 'o') {
 					o++;
-					for(int k = 1; k ==test[i].length ; k++) {
-						if(test[i] == test[k]) {
-						o++;
-						}
-					}
+					sum = sum + o;
 				} else {
 					o = 0;
 				}
-				result = result + 0;
+				
 				
 //				for (int k = 0; k < test[i].length ; k++) { //   (test[j]==0 )이라고 하니 안되었던것. test[][]로 선언했으면 test[][]로 비교해줄것
 //					if(test[i][j] == 'x') {														
 			}
-			System.out.println(result);
+			System.out.println(sum);
 			System.out.println();
 		}
 	
